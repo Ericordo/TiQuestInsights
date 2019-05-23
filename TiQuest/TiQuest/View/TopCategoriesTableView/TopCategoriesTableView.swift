3891499dad4402dc339c959dd1db86bce07e10c2
Eric Ordonneau <eric.ordonneau@outlook.fr>
2019-05-23 13:34:44 +0200

    Worked on tableview

--- TiQuest/TiQuest/View/TopCategoriesTableView/TopCategoriesTableView.swift
+++ TiQuest/TiQuest/View/TopCategoriesTableView/TopCategoriesTableView.swift
@@ -1,49 +1,56 @@
 //
-//  TopCategoriesTableView.swift
+//  TopCategoriesTableViewBis.swift
 //  TiQuest
 //
-//  Created by Eric Ordonneau on 22/05/2019.
+//  Created by Eric Ordonneau on 23/05/2019.
 //  Copyright © 2019 Eric Ordonneau. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class TopCategoriesTableView: UITableView {
-    override init(frame: CGRect, style: UITableView.Style) {
-        super.init(frame: frame, style: .grouped)
-        setUpTableView()
-        
+class TopCategoriesTableView: NSObject {
+    
+   
+    
+    let topCategoriesTableView : UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        return tableView
+    }()
+    
+    
+    func showtopCategories() {
+        if let view = UIApplication.shared.keyWindow {
+            view.addSubview(topCategoriesTableView)
+            topCategoriesTableView.frame = CGRect(x: 10, y: 500, width: 300, height: 300)
+            
+            
+        }
     }
     
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    
+    override init() {
+        super.init()
+        topCategoriesTableView.delegate = self
+        topCategoriesTableView.dataSource = self
+        topCategoriesTableView.register(TopCategoriesTableViewCell.self, forCellReuseIdentifier: "TopCategoryCell")
     }
     
-    
-    
-    func setUpTableView() {
-        self.delegate = self
-        self.dataSource = self
-        self.register(TopCategoriesTableViewCell.self, forCellReuseIdentifier: "TopCategoryCell")
-        
-    }
-    
-
-
 }
 
 extension TopCategoriesTableView: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
     
-func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 10
-}
-
-func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = self.dequeueReusableCell(withIdentifier: "TopCategoryCell", for: indexPath)
-    cell.backgroundColor = .black
-    return cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = topCategoriesTableView.dequeueReusableCell(withIdentifier: "TopCategoryCell", for: indexPath)
+        cell.backgroundColor = .cyan
+        
+      
+        return cell
+        
+    }
+    
     
 }
-
-}
-
