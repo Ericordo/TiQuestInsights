180888bf0704e8946dd25b6c35a2b68e65c36440
Anton C <Antvcl@icloud.com>
2019-05-31 16:19:40 +0200

    Merged project

--- TiQuest/Controller/DashboardViewController.swift
+++ TiQuest/Controller/DashboardViewController.swift
@@ -13,51 +13,110 @@ import Charts
 
 class DashboardViewController: UIViewController {
     
-    let calendarView = Calendar()
+    var calendarView = Calendar()
     let topCategoriesView = TopCategoriesTableView()
-    let topSellersView = TopSellersTableView()
     var todaySalesView : TodaySales!
-  
+    var todaySalesLabel = TodaySalesLabel()
+    let weatherView = WeatherCollectionView()
+    let detailedHourView = DetailedHourView()
     
-
-
-
+    
+    let topSellersView = TopSellersTableView()
+    
+    var exportButton : UIBarButtonItem?
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor(red: 247/255, green: 247/255, blue: 250/255, alpha: 1.0)
+        
         //        MARK: Configuration of the Navigation Bar
         self.navigationItem.title = "Name of your Store"
-        self.navigationItem.leftBarButtonItem = UIBarButtonItem.init(title: "Select", style: .plain, target: self, action: nil)
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem.init(title: "Today", style: .plain, target: self, action: nil)
-    
+        let selectButton = UIBarButtonItem.init(title: "Select", style: .plain, target: self, action: #selector(didTapSelect))
+        let todayButton = UIBarButtonItem.init(title: "Today", style: .plain, target: self, action: #selector(didTapToday))
+        exportButton = UIBarButtonItem.init(barButtonSystemItem: .action, target: self, action: #selector(didTapExport))
+        let logOutButton = UIBarButtonItem.init(title: "Log Out", style: .plain, target: self, action: #selector(didTapLogOut))
+        self.navigationItem.rightBarButtonItems = [logOutButton, exportButton!]
+        self.navigationItem.leftBarButtonItems = [selectButton, todayButton]
         
         
-        // MARK: Configuration of the Today Sales Chart
-        todaySalesView = TodaySales(frame: CGRect(x: 0, y: calendarView.calendarHeight+70, width: view.frame.width, height: 250))
+        //         MARK: Configuration of the Today Sales Chart
+        todaySalesView = TodaySales(frame: CGRect(x: 20, y: calendarView.calendarHeight+71, width: view.frame.width - 20, height: 250))
         view.addSubview(todaySalesView)
         todaySalesView.contentMode = .scaleAspectFit
         TodaySales.playAnimations()
+        view.addSubview(WeekNumberLabel(frame: CGRect(x: 0, y: 71, width: 100, height: 50)))
+        todaySalesLabel.showTodaySalesLabel()
         
+        //         MARK: Configuration of the Weather and Detailed View
+        weatherView.showWeather()
+        detailedHourView.showDetailedHourView()
         
-        print(view.frame.height)
-        
-
-        
-  
-
-        // TODO: Delete line below
-        self.view.backgroundColor = .lightGray
-    }
-    
-    
-    override func viewDidAppear(_ animated: Bool) {
+        //         MARK: Configuration of the Calendar
         calendarView.showCalendar()
         
+        
+        //         MARK: Configuration of the TableViews
+        //        topCategoriesView.showtopCategories()
         topCategoriesView.showTopCategories()
         topCategoriesView.addItems()
         
         topSellersView.showTopSellers()
         topSellersView.addTopSellersItems()
+        
+        
+        
+        
     }
+    
+    
+    
+    
+    @objc func didTapToday() {
+        let lastCellIndexPath = IndexPath.init(row: dates.count-1, section: 0)
+        calendarView.calendarCollectionView.selectItem(at: lastCellIndexPath, animated: true, scrollPosition: .right)
+        TodaySales.playAnimations()
+    }
+    
+    @objc func didTapSelect() {
+        //
+        //        //        TODO: Make a PopOverPresentationController showing the calendar
+        //        let ac = UIAlertController(title: "Hello!", message: "This is a test.", preferredStyle: .actionSheet)
+        //        let popover = ac.popoverPresentationController
+        //        popover?.sourceRect = CGRect(x: 0, y: 0, width: 100, height: 500)
+        //        present(ac, animated: true)
+    }
+    
+    @objc func didTapExport() {
+        let activityViewController = UIActivityViewController(activityItems: [takeScreenshot() as Any], applicationActivities: nil)
+        self.present(activityViewController, animated: true, completion: nil)
+        if let popOver = activityViewController.popoverPresentationController {
+            popOver.sourceView = self.view
+            popOver.barButtonItem = exportButton
+        }
+    }
+    
+    @objc func didTapLogOut() {
+        
+    }
+    
+    open func takeScreenshot(_ shouldSave: Bool = true) -> UIImage? {
+        var screenshotImage :UIImage?
+        let layer = UIApplication.shared.keyWindow!.layer
+        let scale = UIScreen.main.scale
+        UIGraphicsBeginImageContextWithOptions(layer.frame.size, false, scale);
+        guard let context = UIGraphicsGetCurrentContext() else {return nil}
+        layer.render(in:context)
+        screenshotImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return screenshotImage
+    }
+    
+    
     
     
     
@@ -73,13 +132,13 @@ class DashboardViewController: UIViewController {
         print(topSpace)
     }
     
-   
-
     
-
-
-
-
+    
+    
+    
+    
+    
+    
 }
 
 
--- TiQuest/View/Today's Sales/TodaySalesLabel.swift
+++ TiQuest/View/Today's Sales/TodaySalesLabel.swift
@@ -8,28 +8,65 @@
 
 import UIKit
 
-class TodaySalesLabel: UILabel {
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-//    func setUpLabel() {
-//        let todaySalesLabel : UILabel {
-//            
-//        }
-//    }
+func  customizeColor(string: String, color: UIColor) -> NSMutableAttributedString {
+    return NSMutableAttributedString(string: string, attributes:
+        [NSAttributedString.Key.foregroundColor : color ])
+}
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
 
+class TodaySalesLabel: NSObject {
+    override init() {
+        super.init()
+    }
+    
+    
+    
+    
+    let todaySalesLabel : UILabel = {
+        let label = UILabel()
+        var value = 3500
+        //        var litteralValue : NSString = "\(value)€"
+        //        var litteralValue = NSMutableAttributedString(string: "\(value)€", attributes: [NSAttributedString.Key.font: UIFont(name: "Helvetica", size: 15)!, NSAttributedString.Key.foregroundColor: UIColor(cgColor: UIColor.black.cgColor)])
+        //        let description = NSMutableAttributedString(string: "Total sales", attributes: [NSAttributedString.Key.font: UIFont(name: "Helvetica", size: 15)!, NSAttributedString.Key.foregroundColor: UIColor(cgColor: UIColor.lightGray.cgColor)])
+        label.numberOfLines = 2
+        label.backgroundColor = UIColor(red: 247/255, green: 247/255, blue: 250/255, alpha: 0.5)
+        
+        let attrText = customizeColor(string: "\(value) €\n", color: UIColor.black)
+        attrText.append(customizeColor(string: "Total sales", color: UIColor.lightGray))
+        label.attributedText = attrText
+        let horizontalEffect = UIInterpolatingMotionEffect(
+            keyPath: "center.x",
+            type: .tiltAlongHorizontalAxis)
+        horizontalEffect.minimumRelativeValue = -16
+        horizontalEffect.maximumRelativeValue = 16
+        
+        let verticalEffect = UIInterpolatingMotionEffect(
+            keyPath: "center.y",
+            type: .tiltAlongVerticalAxis)
+        verticalEffect.minimumRelativeValue = -16
+        verticalEffect.maximumRelativeValue = 16
+        
+        let effectGroup = UIMotionEffectGroup()
+        effectGroup.motionEffects = [ horizontalEffect,
+                                      verticalEffect ]
+        label.addMotionEffect(effectGroup)
+        return label
+    }()
+    
+    
+    
+    
+    func updateLabel() {
+        
+    }
+    
+    func showTodaySalesLabel() {
+        if let view = UIApplication.shared.keyWindow {
+            view.addSubview(todaySalesLabel)
+            todaySalesLabel.frame = CGRect(x: 70, y: 130, width: 100, height: 50)
+        }
+        
+    }
+    
+    
 }
